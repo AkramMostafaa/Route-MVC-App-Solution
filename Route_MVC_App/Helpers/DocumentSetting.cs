dd1bc8ccd88d031e06119d826ea6133c1a78fd4a
AkramMostafaa <Akrammostafa114@gmail.com>
2024-01-07 16:37:35 +0200

    Implement  Document Setup

---
+++ Route_MVC_App/Helpers/DocumentSetting.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Route_MVC_App.PL.Helpers
+{
+    public class DocumentSetting
+    {
+        public static string UploadFile(IFormFile file,string folderName)
+        {
+            // 1.Get Located Folder Path  
+            //var folderPath = Directory.GetCurrentDirectory()+ @"\wwwroot\files\" + folderName;
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", folderName);
+
+            // 2. Get File Name And Make It UNIQUE
+            string fileName =$"{Guid.NewGuid()}{file.FileName}" ;
+
+            //3. Get File Path
+            string filePath = Path.Combine(folderPath, fileName);
+
+            // 4. Save File AS Streams[Data Per Time ]
+
+            var fileStream = new FileStream(filePath, FileMode.Create); 
+
+            file.CopyTo(fileStream);
+            return fileName;
+
+
+        }
+
+        public static void DeleteFile(string fileName,string folderName) 
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", folderName, fileName);
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            
+        }
+    }
+}
